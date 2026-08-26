2bf6bbfae348a9012e12fc0101c4c773b1124053
ranoot <reynardseow26@gmail.com>
2026-08-26 11:40:52 +0800

    Changed chatbot name from duke to turing

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Turing User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Turing.java
@@ -0,0 +1,10 @@
+public class Turing {
+    public static void main(String[] args) {
+        String banner = " _____ _   _ ____  ___ _   _  ____ \n"
+                + "|_   _| | | |  _ \\|_ _| \\ | |/ ___|\n"
+                + "  | | | | | | |_) || ||  \\| | |  _ \n"
+                + "  | | | |_| |  _ < | || |\\  | |_| |\n"
+                + "  |_|  \\___/|_| \\_\\___|_| \\_|\\____|\n";
+        System.out.println(banner);
+    }
+}
